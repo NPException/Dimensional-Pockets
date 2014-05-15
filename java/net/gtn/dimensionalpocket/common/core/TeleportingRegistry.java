b1b59242edffc6427cdf9058a8a2ad9cbe7bf8ab
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-05-15 15:57:34 +1000

    commit from 05/15/14 15:57:34. 0 deletions, 0 adds, 1 modifications

--- java/net/gtn/dimensionalpocket/common/core/TeleportingRegistry.java
+++ java/net/gtn/dimensionalpocket/common/core/TeleportingRegistry.java
@@ -1,12 +1,18 @@
 package net.gtn.dimensionalpocket.common.core;
 
+import net.minecraft.entity.player.EntityPlayer;
+
 public class TeleportingRegistry {
 
     public static class PlayerState {
-        EntityPlayer
-        
-        public PlayerState(EntityPlayer player) {
+        double x, y, z;
+        String username;
 
+        public PlayerState(EntityPlayer player) {
+            this.x = player.posX;
+            this.y = player.posY;
+            this.z = player.posZ;
+            this.username = player.getDisplayName();
         }
     }
 
