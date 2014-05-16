cb3b47fbc560099898115f33bca432289b72385b
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-05-17 04:09:55 +1000

    commit from 05/17/14 04:09:55. 0 deletions, 1 adds, 0 modifications

---
+++ java/net/gtn/dimensionalpocket/client/UtilFX.java
@@ -0,0 +1,14 @@
+package net.gtn.dimensionalpocket.client;
+
+import net.gtn.dimensionalpocket.common.core.utils.CoordSet;
+import net.minecraft.client.entity.EntityClientPlayerMP;
+
+public class UtilFX {
+
+    public static void createParticleStream(EntityClientPlayerMP player, CoordSet targetSet) {
+
+        
+        
+    }
+
+}
