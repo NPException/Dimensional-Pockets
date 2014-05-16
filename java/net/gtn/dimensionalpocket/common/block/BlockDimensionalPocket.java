cddc3dad2896e17dfd3efa61cde2f2b3edcd3b9c
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-05-16 23:32:58 +1000

    commit from 05/16/14 23:32:58. 0 deletions, 0 adds, 1 modifications

--- java/net/gtn/dimensionalpocket/common/block/BlockDimensionalPocket.java
+++ java/net/gtn/dimensionalpocket/common/block/BlockDimensionalPocket.java
@@ -8,12 +8,14 @@ import net.gtn.dimensionalpocket.common.core.teleport.TeleportingRegistry;
 import net.gtn.dimensionalpocket.common.core.utils.CoordSet;
 import net.gtn.dimensionalpocket.common.core.utils.DPLogger;
 import net.gtn.dimensionalpocket.common.tileentity.TileDimensionalPocket;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockDimensionalPocket extends BlockDP {
@@ -39,8 +41,24 @@ public class BlockDimensionalPocket extends BlockDP {
                 pocket.teleportTo(player);
             }
         }
-
         return true;
+    }
+
+    @Override
+    public int isProvidingStrongPower(IBlockAccess p_149748_1_, int p_149748_2_, int p_149748_3_, int p_149748_4_, int p_149748_5_) {
+        DPLogger.info("Called Strong");
+        return super.isProvidingStrongPower(p_149748_1_, p_149748_2_, p_149748_3_, p_149748_4_, p_149748_5_);
+    }
+
+    @Override
+    public int isProvidingWeakPower(IBlockAccess p_149709_1_, int p_149709_2_, int p_149709_3_, int p_149709_4_, int p_149709_5_) {
+        DPLogger.info("Called Weak");
+        return super.isProvidingWeakPower(p_149709_1_, p_149709_2_, p_149709_3_, p_149709_4_, p_149709_5_);
+    }
+
+    @Override
+    public void onNeighborBlockChange(World world, int x, int y, int z, Block block) {
+
     }
 
     @Override
