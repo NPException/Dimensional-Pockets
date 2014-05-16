414f5ef7265214f7c9a03ecaf9a5ee62bd2ee80c
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-05-17 03:14:46 +1000

    commit from 05/17/14 03:14:46. 0 deletions, 0 adds, 1 modifications

--- java/net/gtn/dimensionalpocket/common/ModItems.java
+++ java/net/gtn/dimensionalpocket/common/ModItems.java
@@ -4,6 +4,7 @@ import net.gtn.dimensionalpocket.common.items.ItemCrafting;
 import net.gtn.dimensionalpocket.common.items.ItemInfoTool;
 import net.gtn.dimensionalpocket.common.lib.Strings;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
@@ -20,7 +21,49 @@ public class ModItems {
 
     public static void initRecipes() {
         CraftingManager crafting = CraftingManager.getInstance();
-        crafting.addRecipe(new ItemStack(ModBlocks.dimensionalPocket), new Object[] { "#N#", "IDI", "#E#", Character.valueOf('#'), new ItemStack(Blocks.stonebrick, 1, 3), Character.valueOf('I'), Blocks.iron_block, Character.valueOf('D'), Blocks.diamond_block, Character.valueOf('N'), new ItemStack(ModItems.craftingItems, 1, 2), Character.valueOf('E'), new ItemStack(ModItems.craftingItems, 1, 1) });
+        //@formatter:off
+        crafting.addRecipe(new ItemStack(ModBlocks.dimensionalPocket, 4), new Object[] { "#N#",
+                                                                                         "IDI",
+                                                                                         "#E#",
+                                                                                         
+                                                                                         Character.valueOf('#'),
+                                                                                         new ItemStack(Blocks.stonebrick, 1, 3),
+                                                                                      
+                                                                                         Character.valueOf('I'),
+                                                                                         Blocks.iron_block,
+                                                                                      
+                                                                                         Character.valueOf('D'),
+                                                                                         Blocks.diamond_block,
+                                                                                      
+                                                                                         Character.valueOf('N'),
+                                                                                         new ItemStack(ModItems.craftingItems, 1, 1),
+                                                                                      
+                                                                                         Character.valueOf('E'),
+                                                                                         new ItemStack(ModItems.craftingItems, 1, 0)
+        });
+        
+        crafting.addRecipe(new ItemStack(ModItems.craftingItems,1,1), new Object[] { "TTT",
+                                                                                     "TRT",
+                                                                                     "TTT",
+                                                                                     
+                                                                                     Character.valueOf('T'),
+                                                                                     Items.ghast_tear,
+                                                                                     
+                                                                                     Character.valueOf('R'),
+                                                                                     Blocks.redstone_block
+        });
+        
+        crafting.addRecipe(new ItemStack(ModItems.craftingItems,1,0), new Object[] { "EEE",
+                                                                                     "EGE",
+                                                                                     "EEE",
+                                                                                     
+                                                                                     Character.valueOf('E'),
+                                                                                     Items.ender_eye,
+                                                                                     
+                                                                                     Character.valueOf('G'),
+                                                                                     Blocks.glass
+        });
+        //@formatter:on
     }
 
 }
