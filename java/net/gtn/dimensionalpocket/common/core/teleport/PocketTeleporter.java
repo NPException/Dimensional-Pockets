fd7c1980581b9b426e79102be9befdac62f27eea
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-05-16 18:53:52 +1000

    commit from 05/16/14 18:53:52. 0 deletions, 0 adds, 1 modifications

--- java/net/gtn/dimensionalpocket/common/core/teleport/PocketTeleporter.java
+++ java/net/gtn/dimensionalpocket/common/core/teleport/PocketTeleporter.java
@@ -22,8 +22,6 @@ public class PocketTeleporter extends Teleporter {
 
     @Override
     public void placeInPortal(Entity entity, double x, double y, double z, float par8) {
-        DPLogger.info(teleportType.name());
-
         if (!(entity instanceof EntityPlayerMP))
             return;
 
@@ -45,35 +43,41 @@ public class PocketTeleporter extends Teleporter {
         CoordSet tempSet = airSet.copy();
 
         int index = 0;
+
         while (!(isAirBlocks(world, tempSet))) {
             tempSet = airSet.copy();
             CoordSet additionSet = getRelativeTries(index++);
-            if (additionSet == null)
-                teleportType = TeleportType.REBOUND;
+            // if (additionSet == null) {
+            // teleportType = TeleportType.REBOUND;
+            // break;
+            // }
             tempSet.addCoordSet(additionSet);
         }
 
-        DPLogger.info(tempSet);
+        if (teleportType == TeleportType.REBOUND) {
+            DPLogger.info("REBOUND");
+            return;
+        }
 
         player.playerNetServerHandler.setPlayerLocation(tempSet.getX() - 0.5F, tempSet.getY() + 1, tempSet.getZ() - 0.5F, player.rotationYaw, player.rotationPitch);
     }
 
-    private boolean isAirBlocks(World world, CoordSet airSet) {
-        if (airSet.getY() < 1)
+    private boolean isAirBlocks(World world, CoordSet tempSet) {
+        DPLogger.info(tempSet);
+        if(tempSet.getY() + 1 < 0){
+            DPLogger.info("WRONGONWONONON");
             return false;
-        return (world.isAirBlock(airSet.getX(), airSet.getY(), airSet.getZ()) && world.isAirBlock(airSet.getX(), airSet.getY() + 1, airSet.getZ()));
+        }
+        return (world.isAirBlock(tempSet.getX(), tempSet.getY(), tempSet.getZ()) && world.isAirBlock(tempSet.getX(), tempSet.getY() + 1, tempSet.getZ()));
     }
 
     private CoordSet getRelativeTries(int tryCount) {
-        if (tryCount == 0)
-            return new CoordSet(0, -2, 0);
-
         int index = 0;
 
         for (int i = -1; i <= 1; i++)
-            for (int j = -2; j <= 2; j++)
+            for (int j = -2; j <= 3; j++)
                 for (int k = -1; k <= 1; k++)
-                    if (++index == tryCount)
+                    if (index++ == tryCount)
                         return new CoordSet(i, j, k);
         return null;
     }
