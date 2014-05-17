a86959d160e27fe5583836ddcd2594c7be436a8d
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-05-18 02:32:30 +1000

    commit from 05/18/14 02:32:30. 0 deletions, 0 adds, 1 modifications

--- java/net/gtn/dimensionalpocket/client/particles/PlayerStreamFX.java
+++ java/net/gtn/dimensionalpocket/client/particles/PlayerStreamFX.java
@@ -1,7 +1,11 @@
 package net.gtn.dimensionalpocket.client.particles;
 
+import static org.lwjgl.opengl.GL11.*;
+
 import net.gtn.dimensionalpocket.client.utils.Colour;
+import net.gtn.dimensionalpocket.client.utils.UtilsFX;
 import net.gtn.dimensionalpocket.common.core.utils.CoordSet;
+import net.gtn.dimensionalpocket.common.lib.Strings;
 import net.minecraft.client.particle.EntityFX;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.entity.player.EntityPlayer;
@@ -13,7 +17,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class PlayerStreamFX extends EntityFX {
 
     private static final Colour colour = new Colour(1.0F, 1.0F, 1.0F, 0.5F);
-    
+
     public PlayerStreamFX(World world, EntityPlayer player, CoordSet targetSet, int ticksToTake) {
         super(world, player.posX, player.posY, player.posZ);
         noClip = true;
@@ -40,11 +44,31 @@ public class PlayerStreamFX extends EntityFX {
     public int getFXLayer() {
         return 2;
     }
-    
+
     @Override
-    public void renderParticle(Tessellator tessellator, float par2, float par3, float par4, float par5, float par6, float par7) {
+    public void renderParticle(Tessellator tessellator, float tick, float par3, float par4, float par5, float par6, float par7) {
         tessellator.draw();
-        
-        
+
+        glPushMatrix();
+        glDepthMask(false);
+        glEnable(3042);
+        glBlendFunc(770, 771);
+
+        UtilsFX.bindTexture(Strings.DIMENSIONAL_POCKET_PARTICLE);
+
+        colour.doGL();
+
+        tessellator.startDrawingQuads();
+        tessellator.setBrightness(240);
+
+        float interpX = (float) (prevPosX + (posX - prevPosX) * tick - interpPosX);
+        float interpY = (float) (prevPosY + (posY - prevPosY) * tick - interpPosY);
+        float interpZ = (float) (prevPosZ + (posZ - prevPosZ) * tick - interpPosZ);
+
+        tessellator.draw();
+
+        glPopMatrix();
+
+        tessellator.startDrawingQuads();
     }
 }
