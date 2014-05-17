84dddcc1c6d05f8abdc7e20b4cc00f1e14e96388
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-05-18 06:03:03 +1000

    commit from 05/18/14 06:03:03. 0 deletions, 0 adds, 1 modifications

--- java/net/gtn/dimensionalpocket/client/gui/GuiInfoBook.java
+++ java/net/gtn/dimensionalpocket/client/gui/GuiInfoBook.java
@@ -14,7 +14,7 @@ public class GuiInfoBook extends GuiContainer {
                 return true;
             }
         });
-        
+
         xSize = 154;
         ySize = 180;
     }
@@ -23,9 +23,8 @@ public class GuiInfoBook extends GuiContainer {
     public void initGui() {
         super.initGui();
 
-        
     }
-    
+
     @Override
     public void drawDefaultBackground() {
     }
@@ -38,6 +37,26 @@ public class GuiInfoBook extends GuiContainer {
         mc.renderEngine.bindTexture(GuiSheet.GUI_INFO_BOOK);
 
         drawTexturedModalRect(x, y, 12, 1, xSize, ySize);
+
+        drawArrow(1);
+        drawArrow(-1);
+    }
+
+    private void drawArrow(int direction) {
+        int tempXSize = 18;
+        int tempYSize = 10;
+
+        int x = (width - tempXSize) / 2 + 2;
+        int y = (height - tempYSize) / 2 - 2;
+
+        int translateX = 44 * direction;
+        x += translateX;
+
+        int yPos = 194;
+        if (direction == -1)
+            yPos += 13;
+
+        drawTexturedModalRect(x, y + (ySize / 2) - tempXSize, 3, yPos, tempXSize, tempYSize);
     }
 
 }
