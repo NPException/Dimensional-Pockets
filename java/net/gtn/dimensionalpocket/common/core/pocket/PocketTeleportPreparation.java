49f201916db4f3f6dd4832b34410660b0fc5566f
NPException <npexception42@gmail.com>
2014-05-18 00:38:13 +0200

    time 00:38 - pushe teleport prep

--- java/net/gtn/dimensionalpocket/common/core/pocket/PocketTeleportPreparation.java
+++ java/net/gtn/dimensionalpocket/common/core/pocket/PocketTeleportPreparation.java
@@ -1,12 +1,11 @@
-package net.gtn.dimensionalpocket.common.core.utils;
+package net.gtn.dimensionalpocket.common.core.pocket;
 
-import net.gtn.dimensionalpocket.common.core.pocket.Pocket;
 import net.minecraft.entity.player.EntityPlayer;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 @SideOnly(Side.SERVER)
-public class TeleportPreparation {
+public class PocketTeleportPreparation {
     public enum Direction {
         INTO_POCKET,
         OUT_OF_POCKET
@@ -17,7 +16,7 @@ public class TeleportPreparation {
     private final Pocket pocket;
     private final Direction direction;
     
-    public TeleportPreparation(EntityPlayer player, int ticksBeforeTeleport, Pocket pocket, Direction direction) {
+    public PocketTeleportPreparation(EntityPlayer player, int ticksBeforeTeleport, Pocket pocket, Direction direction) {
         this.player = player;
         this.ticksBeforeTeleport = ticksBeforeTeleport;
         this.pocket = pocket;
