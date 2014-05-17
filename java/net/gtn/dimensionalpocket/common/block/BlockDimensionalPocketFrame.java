c2177205e4528f8d799936783f46642a38240114
Jezzadabomb338 <jezzadabomb@gmail.com>
2014-05-17 17:19:41 +1000

    commit from 05/17/14 17:19:41. 0 deletions, 0 adds, 1 modifications

--- java/net/gtn/dimensionalpocket/common/block/BlockDimensionalPocketFrame.java
+++ java/net/gtn/dimensionalpocket/common/block/BlockDimensionalPocketFrame.java
@@ -41,12 +41,7 @@ public class BlockDimensionalPocketFrame extends BlockDP {
     }
 
     @Override
-    public boolean canConnectRedstone(IBlockAccess world, int x, int y, int z, int side) {
-        return true;
-    }
-
-    @Override
-    public boolean canProvidePower() {
+    public boolean isNormalCube() {
         return true;
     }
 
@@ -68,10 +63,6 @@ public class BlockDimensionalPocketFrame extends BlockDP {
 
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitVecX, float hitVecY, float hitVecZ) {
-        boolean tru = true;
-        if (tru)
-            return false;
-
         if (player == null)
             return false;
 
