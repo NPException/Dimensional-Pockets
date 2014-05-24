e78951cba8e3d8dd79b72179de8478d8795644d9
NPException <npexception42@gmail.com>
2014-05-24 10:10:16 +0200

    This is a good point to up increment the version imho

--- java/net/gtn/dimensionalpocket/common/lib/Reference.java
+++ java/net/gtn/dimensionalpocket/common/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
     public static final String MOD_ID = "dimensionalPockets";
     public static final String MOD_NAME = "Dimensional Pockets";
-    public static final String VERSION = "v0.0.01";
+    public static final String VERSION = "0.07";
 
     public static final String MOD_IDENTIFIER = MOD_ID + ":";
 
