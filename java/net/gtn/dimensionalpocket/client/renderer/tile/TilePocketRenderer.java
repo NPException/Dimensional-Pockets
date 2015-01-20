5f186f7423ba69b8a5bc6b730b793730b4471802
NPException <npexception42@gmail.com>
2015-01-20 18:09:06 +0100

    temporary renderer changes

--- java/net/gtn/dimensionalpocket/client/renderer/tile/TilePocketRenderer.java
+++ java/net/gtn/dimensionalpocket/client/renderer/tile/TilePocketRenderer.java
@@ -1,5 +1,10 @@
 package net.gtn.dimensionalpocket.client.renderer.tile;
 
+import static org.lwjgl.opengl.GL11.*;
+
+import java.nio.FloatBuffer;
+import java.util.Random;
+
 import net.gtn.dimensionalpocket.common.lib.Reference;
 import net.gtn.dimensionalpocket.common.tileentity.TileDimensionalPocket;
 import net.minecraft.client.Minecraft;
@@ -10,11 +15,6 @@ import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
-
-import java.nio.FloatBuffer;
-import java.util.Random;
-
-import static org.lwjgl.opengl.GL11.*;
 
 public class TilePocketRenderer extends TileEntitySpecialRenderer {
     FloatBuffer floatBuffer = GLAllocation.createDirectFloatBuffer(16);
@@ -41,11 +41,11 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
         double maxDistance = 16.0; // distance to block
         this.inRange = Minecraft.getMinecraft().renderViewEntity.getDistanceSq(tile.xCoord + 0.5D, tile.yCoord + 0.5D, tile.zCoord + 0.5D) < (maxDistance * maxDistance);
         glPushMatrix();
-        glDisable(2912);
+        glDisable(GL_FOG);
 
         // Y Neg
         drawPlane(0, x, y, z, 0.001F);
-//        Y Pos
+        // Y Pos
         drawPlane(1, x, y, z, 0.999F);
         // Z Neg
         drawPlane(2, x, y, z, 0.001F);
@@ -56,11 +56,11 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
         // X Pos
         drawPlane(5, x, y, z, 0.999F);
 
-        glDisable(2896);
+        glDisable(GL_LIGHTING);
         bindTexture(pocketFrame);
 
-        glEnable(3042);
-        glBlendFunc(770, 771);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         Tessellator instance = Tessellator.instance;
 
         if (tile.getWorldObj() != null)
@@ -69,12 +69,12 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
             instance.setBrightness(220);
 
         float greyScale = 1.0F;
-        instance.setColorRGBA_F(greyScale, greyScale, greyScale, 1.0F);
+        instance.setColorRGBA_F(greyScale, greyScale, greyScale, 1F);
 
         renderFaces(instance, x, y, z);
 
         // TODO: NPE Here's the shit you need to worry about.
-        /**
+        /*
          * Basically it's all here, the glColor3f() will set the colour, all you need to do is grab it with tile.getShit(); and it'll work.
          */
 //        float red = System.currentTimeMillis() % 700000L / 250000.0F;
@@ -82,17 +82,18 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
 //        float blue = System.currentTimeMillis() % 700000L / 250000.0F;
 //
 //        glColor3f(red, green, blue);
+        
         bindTexture(pocketOverlay);
         renderFaces(instance, x, y, z);
 
-        glDisable(3042);
+        glDisable(GL_BLEND);
 
-        glEnable(2896);
-        glEnable(2912);
+        glEnable(GL_LIGHTING);
+        glEnable(GL_FOG);
         glPopMatrix();
     }
 
-    private void renderFaces(Tessellator instance, double x, double y, double z) {
+    private static void renderFaces(Tessellator instance, double x, double y, double z) {
         instance.startDrawingQuads();
         // @formatter:off
 		// Y Neg
@@ -134,7 +135,9 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
         float dX = (float) TileEntityRendererDispatcher.staticPlayerX;
         float dY = (float) TileEntityRendererDispatcher.staticPlayerY;
         float dZ = (float) TileEntityRendererDispatcher.staticPlayerZ;
-        glDisable(2896);
+        
+        glPushMatrix();
+        glDisable(GL_LIGHTING);
         random.setSeed(31100L);
         if (inRange) {
             switch (side) {
@@ -161,12 +164,13 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
             renderOutOfRange(side, x, y, z, offset);
         }
 
-        glDisable(3042);
-        glDisable(3168);
-        glDisable(3169);
-        glDisable(3170);
-        glDisable(3171);
-        glEnable(2896);
+        glDisable(GL_BLEND);
+        glDisable(GL_TEXTURE_GEN_S);
+        glDisable(GL_TEXTURE_GEN_T);
+        glDisable(GL_TEXTURE_GEN_R);
+        glDisable(GL_TEXTURE_GEN_Q);
+        glEnable(GL_LIGHTING);
+        glPopMatrix();
     }
 
     public void renderOutOfRange(int side, double x, double y, double z, float offset) {
@@ -238,12 +242,12 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
                 f7 = 0.1F;
                 f5 = 65.0F;
                 f6 = 0.125F;
-                glEnable(3042);
-                glBlendFunc(770, 771);
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             }
             if (tessellator == 1) {
                 bindTexture(particleField);
-                glEnable(3042);
+                glEnable(GL_BLEND);
                 glBlendFunc(1, 1);
                 f6 = 0.5F;
             }
@@ -311,12 +315,12 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
                 f7 = 0.1F;
                 f5 = 65.0F;
                 f6 = 0.125F;
-                glEnable(3042);
-                glBlendFunc(770, 771);
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             }
             if (tessellator == 1) {
                 bindTexture(particleField);
-                glEnable(3042);
+                glEnable(GL_BLEND);
                 glBlendFunc(1, 1);
                 f6 = 0.5F;
             }
@@ -384,12 +388,12 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
                 f7 = 0.1F;
                 f5 = 65.0F;
                 f6 = 0.125F;
-                glEnable(3042);
-                glBlendFunc(770, 771);
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             }
             if (tessellator == 1) {
                 bindTexture(particleField);
-                glEnable(3042);
+                glEnable(GL_BLEND);
                 glBlendFunc(1, 1);
                 f6 = 0.5F;
             }
@@ -457,12 +461,12 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
                 f7 = 0.1F;
                 f5 = 65.0F;
                 f6 = 0.125F;
-                glEnable(3042);
-                glBlendFunc(770, 771);
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             }
             if (tessellator == 1) {
                 bindTexture(particleField);
-                glEnable(3042);
+                glEnable(GL_BLEND);
                 glBlendFunc(1, 1);
                 f6 = 0.5F;
             }
@@ -530,12 +534,12 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
                 f7 = 0.1F;
                 f5 = 65.0F;
                 f6 = 0.125F;
-                glEnable(3042);
-                glBlendFunc(770, 771);
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             }
             if (tessellator == 1) {
                 bindTexture(particleField);
-                glEnable(3042);
+                glEnable(GL_BLEND);
                 glBlendFunc(1, 1);
                 f6 = 0.5F;
             }
@@ -603,12 +607,12 @@ public class TilePocketRenderer extends TileEntitySpecialRenderer {
                 f7 = 0.1F;
                 f5 = 65.0F;
                 f6 = 0.125F;
-                glEnable(3042);
-                glBlendFunc(770, 771);
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             }
             if (tessellator == 1) {
                 bindTexture(particleField);
-                glEnable(3042);
+                glEnable(GL_BLEND);
                 glBlendFunc(1, 1);
                 f6 = 0.5F;
             }
