875ee68f3c9978f200253343d6779868791315fa
Jezzadabomb338 <jezzadabomb@gmail.com>
2015-01-21 08:57:32 +1100

    Fixed derp

--- java/net/gtn/dimensionalpocket/client/utils/UtilsFX.java
+++ java/net/gtn/dimensionalpocket/client/utils/UtilsFX.java
@@ -5,16 +5,35 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import me.jezza.oc.common.utils.CoordSet;
 import net.gtn.dimensionalpocket.client.particles.PlayerStreamFX;
+import net.gtn.dimensionalpocket.common.lib.Reference;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.EffectRenderer;
 import net.minecraft.client.particle.EntityFX;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @SideOnly(Side.CLIENT)
 public class UtilsFX {
+
+    private static Map<String, ResourceLocation> boundTextures = new HashMap<String, ResourceLocation>();
+
+    public static void bindTexture(String texture) {
+        ResourceLocation resource = null;
+        String key = "textures/misc/" + texture + ".png";
+
+        if (boundTextures.containsKey(key)) {
+            resource = boundTextures.get(key);
+        } else {
+            resource = new ResourceLocation(Reference.MOD_IDENTIFIER + key);
+            boundTextures.put(texture, resource);
+        }
+
+        Minecraft.getMinecraft().renderEngine.bindTexture(resource);
+    }
 
     public static void createPlayerStream(EntityPlayer player, CoordSet targetSet, int ticksToTake) {
         EntityFX fx = new PlayerStreamFX(player.worldObj, player, targetSet, ticksToTake, new Random(), new Colour(0.1F, 0.1F, 0.1F, 0.5F));
