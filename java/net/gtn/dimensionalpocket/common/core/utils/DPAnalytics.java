3dfe6365ab8deb96693610e00f4c67cda20cb679
NPException <npexception42@gmail.com>
2015-08-09 16:43:09 +0200

    fixed duplicate crafting GA event

--- java/net/gtn/dimensionalpocket/common/core/utils/DPAnalytics.java
+++ java/net/gtn/dimensionalpocket/common/core/utils/DPAnalytics.java
@@ -22,8 +22,8 @@ import de.npe.gameanalytics.minecraft.MCSimpleAnalytics;
  *
  */
 public class DPAnalytics extends MCSimpleAnalytics {
-	public static final String GA_GAME_KEY = "12345";
-	public static final String GA_SECRET_KEY = "12345";
+	public static final String GA_GAME_KEY = "da1d2ad5b654b795d187f0a2cc8d0609";
+	public static final String GA_SECRET_KEY = "760658a7191e5d29f3a6cc9f8ad325ed88631535";
 
 	public static DPAnalytics analytics;
 
