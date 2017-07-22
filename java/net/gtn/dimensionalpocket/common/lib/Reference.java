a17775e212268623c6e29aa507e445a272535d77
NPException <npexception42@gmail.com>
2017-07-22 17:03:45 +0200

    Bumped Version to 1.0.3

--- java/net/gtn/dimensionalpocket/common/lib/Reference.java
+++ java/net/gtn/dimensionalpocket/common/lib/Reference.java
@@ -10,7 +10,7 @@ public class Reference {
 
 	public static final String MOD_ID = "dimensionalPockets";
 	public static final String MOD_NAME = "Dimensional Pockets";
-	public static final String VERSION = "1.0.2";
+	public static final String VERSION = "1.0.3";
 
 	public static final String MOD_IDENTIFIER = MOD_ID + ":";
 
