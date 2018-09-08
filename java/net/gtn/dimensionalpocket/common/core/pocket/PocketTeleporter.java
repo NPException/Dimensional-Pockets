a827e461a0ed6cb81d57d50aa17fc960f54dd854
NPException <npexception42@gmail.com>
2018-09-08 21:27:04 +0200

    custom teleport code
    closed #107
    
    - copied the teleport code from ServerConfigurationManager
    and removed all dimension dependent modifications to
    the player target position
    
    - bumped version to 1.0.5

--- java/net/gtn/dimensionalpocket/common/core/pocket/PocketTeleporter.java
+++ java/net/gtn/dimensionalpocket/common/core/pocket/PocketTeleporter.java
@@ -1,18 +1,26 @@
 package net.gtn.dimensionalpocket.common.core.pocket;
 
-import net.gtn.dimensionalpocket.oc.common.utils.CoordSet;
+import java.util.Collection;
+
+import cpw.mods.fml.common.FMLCommonHandler;
 import net.gtn.dimensionalpocket.common.core.utils.Utils;
+import net.gtn.dimensionalpocket.oc.common.utils.CoordSet;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.play.server.S07PacketRespawn;
+import net.minecraft.network.play.server.S1DPacketEntityEffect;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.management.ServerConfigurationManager;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
 
 
 public class PocketTeleporter extends Teleporter {
 
-	CoordSet targetSet;
-	float spawnYaw, spawnPitch;
+	private CoordSet targetSet;
+	private float spawnYaw, spawnPitch;
 
 	public PocketTeleporter(WorldServer worldServer, CoordSet targetSet, float spawnYaw, float spawnPitch) {
 		super(worldServer);
@@ -24,8 +32,9 @@ public class PocketTeleporter extends Teleporter {
 
 	@Override
 	public void placeInPortal(Entity entity, double x, double y, double z, float par8) {
-		if (!(entity instanceof EntityPlayerMP))
+		if (!(entity instanceof EntityPlayerMP)) {
 			return;
+		}
 
 		EntityPlayerMP player = (EntityPlayerMP) entity;
 
@@ -37,11 +46,72 @@ public class PocketTeleporter extends Teleporter {
 		player.fallDistance = 0;
 	}
 
-	public static PocketTeleporter createTeleporter(int dimID, CoordSet coordSet, float spawnYaw, float spawnPitch) {
+	static PocketTeleporter createTeleporter(int dimID, CoordSet coordSet, float spawnYaw, float spawnPitch) {
 		return new PocketTeleporter(MinecraftServer.getServer().worldServerForDimension(dimID), coordSet, spawnYaw, spawnPitch);
 	}
 
-	public static void transferPlayerToDimension(EntityPlayerMP player, int dimID, Teleporter teleporter) {
-		MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension(player, dimID, teleporter);
+	/**
+	 * Stripped down version of {@link ServerConfigurationManager#transferPlayerToDimension(EntityPlayerMP, int, Teleporter)}
+	 * which doesn't do any special handling for teleports between dimensions.
+	 * (like moving to spawn when teleporting from the End, or applying movement factors when teleporting to the Nether)
+	 */
+	public static void transferPlayerToDimension(EntityPlayerMP player, int targetDim, Teleporter teleporter) {
+		int sourceDim = player.dimension;
+		MinecraftServer mcServer = MinecraftServer.getServer();
+		WorldServer sourceWorld = mcServer.worldServerForDimension(player.dimension);
+		player.dimension = targetDim;
+		WorldServer targetWorld = mcServer.worldServerForDimension(player.dimension);
+		player.playerNetServerHandler.sendPacket(
+				new S07PacketRespawn(player.dimension, targetWorld.difficultySetting,
+						targetWorld.getWorldInfo().getTerrainType(), player.theItemInWorldManager.getGameType()));
+		sourceWorld.removePlayerEntityDangerously(player);
+		player.isDead = false;
+
+		transferEntityToWorld(player, sourceWorld, targetWorld, teleporter);
+
+		ServerConfigurationManager serverConfigurationManager = MinecraftServer.getServer().getConfigurationManager();
+		// remove player from source world and add to target world. load chunk with player
+		serverConfigurationManager.func_72375_a(player, sourceWorld);
+		player.playerNetServerHandler.setPlayerLocation(player.posX, player.posY, player.posZ, player.rotationYaw, player.rotationPitch);
+		player.theItemInWorldManager.setWorld(targetWorld);
+
+		serverConfigurationManager.updateTimeAndWeatherForPlayer(player, targetWorld);
+		serverConfigurationManager.syncPlayerInventory(player);
+
+		@SuppressWarnings("unchecked")
+		Collection<PotionEffect> potionEffects = player.getActivePotionEffects();
+
+		for (PotionEffect potioneffect : potionEffects) {
+			player.playerNetServerHandler.sendPacket(new S1DPacketEntityEffect(player.getEntityId(), potioneffect));
+		}
+		FMLCommonHandler.instance().firePlayerChangedDimensionEvent(player, sourceDim, targetDim);
+	}
+
+	/**
+	 * Stripped down version of {@link ServerConfigurationManager#transferEntityToWorld(Entity, int, WorldServer, WorldServer, Teleporter)}
+	 * which does no special handling for teleports from an to the end.
+	 */
+	private static void transferEntityToWorld(Entity entityIn, WorldServer sourceWorld, WorldServer targetWorld, Teleporter teleporter) {
+		double d0 = entityIn.posX;
+		double d1 = entityIn.posZ;
+		double d3 = entityIn.posX;
+		double d4 = entityIn.posY;
+		double d5 = entityIn.posZ;
+		float f = entityIn.rotationYaw;
+
+		sourceWorld.theProfiler.startSection("placing");
+		d0 = MathHelper.clamp_int((int) d0, -29999872, 29999872);
+		d1 = MathHelper.clamp_int((int) d1, -29999872, 29999872);
+
+		if (entityIn.isEntityAlive()) {
+			entityIn.setLocationAndAngles(d0, entityIn.posY, d1, entityIn.rotationYaw, entityIn.rotationPitch);
+			teleporter.placeInPortal(entityIn, d3, d4, d5, f);
+			targetWorld.spawnEntityInWorld(entityIn);
+			targetWorld.updateEntityWithOptionalForce(entityIn, false);
+		}
+
+		sourceWorld.theProfiler.endSection();
+
+		entityIn.setWorld(targetWorld);
 	}
 }
